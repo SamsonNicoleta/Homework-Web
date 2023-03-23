2856a550b63a0993cc0af51f62ce2b3539bd8f70
SamsonNicoleta <nicoletasamson194@gmail.com>
2023-03-23 09:33:45 +0200

    3.5.2

---
+++ Ex3(3.5.2).js
@@ -0,0 +1,16 @@
+•	let menu = {
+    •	  width: 200,
+    •	  height: 300,
+    •	  title: "My menu"
+    •	};
+    •	multiplyNumeric(menu);
+    •	
+    •	function multiplyNumeric(obj) {
+    •	  for(let key in obj){
+    •	
+    •	    (typeof(obj[key]) === 'number') ? (obj[key] *= 2) : (false); //typeof controleaza daca obiectul este numar apoi se dubleaza numarul
+    •	  }
+    •	}
+    •	
+    •	console.log(menu);
+    
